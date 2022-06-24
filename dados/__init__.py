4c3fc57bf044d4340457a1bfb149b73a059c7eec
Symphoriano Martins Branco <104242815+SymMartins@users.noreply.github.com>
2022-06-24 15:59:58 -0300

    Iniciando o Back-end. Uma parte do cadastro de Motoristas está pronto!

--- dados/__init__.py
+++ dados/__init__.py
@@ -1,3 +1,4 @@
+from interface import *
 
 def dadosMotorista(nome):
     try:
@@ -8,3 +9,42 @@ def dadosMotorista(nome):
     else:
         return True
 
+
+def criaDadosMotorista(nome):
+    try:
+        a = open(nome, 'wt+')
+        a.close()
+    except:
+        print('Erro ao criar o arquivo!')
+    else:
+        print(f'O aquivo {nome} foi criado com sucesso!')
+
+
+def cadastraMotorista(dadosMotorista, nome, end, tel, cnh):
+    try:
+        a = open(dadosMotorista, 'at')
+    except:
+        print(f'ERRO ao abrir o arquivo {dadosMotorista}!')
+    else:
+        try:
+            a.write(f'{nome}; {end}; {tel}; {cnh}\n')
+        except:
+            print('ERRO! Ao escrever os dados!')
+        else:
+            print(f'O motorista: {nome} foi cadastrado.')
+            a.close()
+
+
+def lerDadosMotoristas(dadosMotorista):
+    try:
+        a = open(dadosMotorista, 'rt')
+    except:
+        print(f'ERRO ao ler o arquivo {dadosMotorista}')
+    else:
+        cabecalho('MOTORISTAS CADASTRADOS')
+        for linha in a:
+            dados = linha.split(';')
+            dados[1] = dados[1].replace('\n', '')
+            print(f'Nome:{dados[0]:<5} - End:{dados[1]:<5} - Tel:{dados[2]:<5} - CNH:{dados[3]:>5}')
+    finally:
+        a.close()
--- sistema.py
+++ sistema.py
@@ -1,20 +1,33 @@
 from interface import *
 from dados import *
+from time import sleep
+
 dados_motorista = 'dadosmotorista.txt'
 
-"""if not dadosMotorista(dados_motorista):
-    criarArquivo(dados_motorista)"""
-
 while True:
-    resposta = menu(['Cadastrar Motoristas', 'Cadastrar Veículos', 'Sair do Sistema'])
+    resposta = menu(['Cadastrar Motoristas', 'Cadastrar Veículos', 'Motoristas Cadastrados', 'Veículos Cadastrados',
+                     'Sair do Sistema'])
     if resposta == 1:
-        print('Opção 1')
+        if not dadosMotorista(dados_motorista):
+            criaDadosMotorista(dados_motorista)
+
+        cabecalho('CADASTRO DE MOTORISTA')
+        nome = str(input('Nome: ')).title().strip()
+        end = str(input('End: ')).title().strip()
+        tel = int(input('Tel: '))
+        cnh = int(input('CNH: '))
+        cadastraMotorista(dados_motorista, nome, end, tel, cnh)
+
     elif resposta == 2:
-        print('Opção 2')
+        cabecalho('CADASTRO DE VEÍCULOS')
     elif resposta == 3:
-        print('Opção 3')
+        lerDadosMotoristas(dados_motorista)
+    elif resposta == 4:
+        print('Opção 4')
+    elif resposta == 5:
+        cabecalho('Saindo do sistema...')
         break
     else:
         print('ERRO!! Digite uma opção válida')
-
+    sleep(1)
 
